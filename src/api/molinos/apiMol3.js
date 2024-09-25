d2f394040b7fdb96f0f0a304ac93966927ff301c
Brenda-Dridka <ruiz83636@gmail.com>
2024-09-25 13:50:26 -0600

    actualizacion

--- src/api/molinos/apiMol3.js
+++ src/api/molinos/apiMol3.js
@@ -7,7 +7,7 @@ export const fetchEtiquetasMol3 = async () => {
   const response = await axios.get(apiUrlEtiquetasMol3);
   return response.data;
 };
-
+/*  */
 export const createEtiquetaMol3 = async (etiquetaMol3Data) => {
   const response = await axios.post(apiUrlEtiquetasMol3, etiquetaMol3Data);
   return response.data;
